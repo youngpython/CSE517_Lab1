078412acfe6a19f134677c425055e22550cd4f77
Heather Berlin <heatherberlin33@gmail.com>
2015-02-02 16:55:44 -0600

    updated distance function and tried breaking ties

--- knnclassifier.m
+++ knnclassifier.m
@@ -18,17 +18,43 @@ knn_predicted_labels = yTr(indices);
 if (size(knn_predicted_labels,1)==1)
     preds = knn_predicted_labels;
 else
-    preds = mode(knn_predicted_labels); 
+
+   % label_count = label_count(:,2:size(label_count));
+    
+    %sorted_label_counts = sort(label_count,2,'descend');
+   % sorted_label_counts = vertcat(uniqu
+    %bool noclearmode = TRUE;
+    
+        
+    %max_num_same_label = max(label_count(:,2:size(label_count(:,2))));
+    %max_num_same_label = repmat(max_num_same_label,size(unique_labels));
+    %num_of_labels_that_satisfy_mode;
+    %[a b c]= find(label_count == max_num_same_label);
+    %if (length(num_of_labels_that_satisfy_mode)>1)
+    %    preds(i) = knn_predicted_labels(i);
+    %end;
+    
+    %no clear mode!
+    %fall back on k=1
+    %sort label counts in descending order
+    %if first equals second, fall back to k=1 prediction choice
+    %preds(i) = knn_predicted_labels;
+    preds = mode(knn_predicted_labels);
 end;
 
-
-
-% output random result as default (you can erase this code)
-%[d,n]=size(xTe);
-%[d,ntr]=size(xTr);
-%if k>ntr,k=ntr;end;
-%un=unique(yTr);
-%preds=un(ceil(rand(1,n)*length(un)));
-
+    %check if modes are all the same for negative matrices (mode finds
+    %most occuring value <- takes the minimum one
+    %*mode_using_pos_vals = mode(knn_predicted_labels);
+    %negative_preds = -1*knn_predicted_labels;
+    %negative_mode = mode(-1*knn_predicted_labels)
+    %mode_using_neg_vals = -1*negative_mode;
+    %[ a b] = 
+    
+  %  if (mode_using_neg_vals(1,:) ~= mode_using_pos_vals(1,:))
+   %     preds(1,:)=knn_predicted_labels(1,:);
+  %  end;
+    
+    
+   
 %% fill in code here
 
